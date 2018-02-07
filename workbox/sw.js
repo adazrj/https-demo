5a5097ab97287d353ae5b837d625ef06a9d1b622
ada <819591370@qq.com>
2018-02-07 23:43:09 +0800

    change

--- workbox/sw.js
+++ workbox/sw.js
@@ -1,20 +1,18 @@
 // workbox 2.x 是将 workbox 核心内容放在 workbox-sw node_modules 包里维护的
 // workbox 3.x 开始是将 workbox 核心 lib 放在 CDN 维护
 // 当然也可以挪到自己的 CDN 维护
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-alpha.3/workbox-sw.js');
+importScripts('./build/workbox-sw.js');
 
-if (workbox) {
-    console.log('Hello from sw.js');
-    workbox.routing.registerRoute(
-      new RegExp('./static/*\.js'),
-      workbox.strategies.networkFirst()
-    );
-
-}
-else {
-    console.log("Workbox didn't loaded ");
-}
-
+const workboxSW = new WorkboxSW();
+workboxSW.precache([
+  {
+    url: '/index.html',
+    revision: 'bb121c',
+  }, {
+    url: './static/mm1.jpg',
+    revision: 'acd123',
+  }
+]);
 
 
 
