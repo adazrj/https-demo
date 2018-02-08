382cc84cd3b8e02c771139deedc0128d54455441
ada <819591370@qq.com>
2018-02-08 08:40:33 +0800

    sw

--- workbox3/sw.js
+++ workbox3/sw.js
@@ -1,17 +1,13 @@
 // workbox 2.x 是将 workbox 核心内容放在 workbox-sw node_modules 包里维护的
 // workbox 3.x 开始是将 workbox 核心 lib 放在 CDN 维护
 // 当然也可以挪到自己的 CDN 维护
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-alpha.3/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-beta.0/workbox-sw.js');
 
-const workboxSW = new WorkboxSW();
-workboxSW.precache([
-  {
-    url: './static/mm1.jpg',
-    revision: 'acd123',
-  }
-]);
-
-
+if (workbox) {
+  console.log('Yay! Workbox is loaded');
+} else {
+  console.log("Boo! Workbox didn't load");
+}
 
 
 
