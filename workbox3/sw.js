fc0c5e31641f3d1114f10580b361c79d85fc4b07
adarzheng <819591370@qq.com>
2018-02-08 11:51:41 +0800

    bug

--- workbox3/sw.js
+++ workbox3/sw.js
@@ -5,12 +5,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-beta.0/
 
 if (workbox) {
   console.log('Yay! Workbox is loaded');
-  workbox.routing.registerRoute(
-	  new RegExp('.*\.html'),workbox.strategies.networkFirst()
-	);
-  workbox.routing.registerRoute(
-	  new RegExp('.*\.js'),workbox.strategies.networkFirst()
-	);
+  
   workbox.routing.registerRoute(
 	  // Cache CSS files
 	  /.*\.css/,
