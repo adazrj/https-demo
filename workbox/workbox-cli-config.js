0c93d7a1159e524384d7068f6b5e79a2cc3119e6
adarzheng <819591370@qq.com>
2018-02-08 13:44:46 +0800

    debug

---
+++ workbox/workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": "static/",
+  "globPatterns": [
+    "**/*.{js,jpg}"
+  ],
+  "swDest": "build/sw.js",
+  "globIgnores": [
+    "../workbox-cli-config.js"
+  ]
+};
