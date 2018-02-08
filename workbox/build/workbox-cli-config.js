20eca8d4bfba5a9b6d8688413f5ab111cf7abeed
adarzheng <819591370@qq.com>
2018-02-08 20:52:40 +0800

    debug

---
+++ workbox/build/workbox-cli-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "globDirectory": "../",
+  "globPatterns": [
+    "**/*.{js,map,html,jpg}"
+  ],
+  "swSrc": "../sw.js",
+  "swDest": "../sw.js",
+  "globIgnores": [
+    "build/workbox-cli-config.js"
+  ]
+};
--- workbox/sw.js
+++ workbox/sw.js
@@ -1,32 +1,38 @@
-importScripts('./build/workbox-sw.js');
-
-/**
- * DO NOT EDIT THE FILE MANIFEST ENTRY
- *
- * The method precache() does the following:
- * 1. Cache URLs in the manifest to a local cache.
- * 2. When a network request is made for any of these URLs the response
- *    will ALWAYS comes from the cache, NEVER the network.
- * 3. When the service worker changes ONLY assets with a revision change are
- *    updated, old cache entries are left as is.
- *
- * By changing the file manifest manually, your users may end up not receiving
- * new versions of files because the revision hasn't changed.
- *
- * Please use workbox-build or some other tool / approach to generate the file
- * manifest which accounts for changes to local files and update the revision
- * accordingly.
- */
-const fileManifest = [
+workboxSW.precache([
   {
-    "url": "jquery.min.js",
+    "url": "build/sw.js",
+    "revision": "57d9fde4fd4e7494fe645f8cc373e53d"
+  },
+  {
+    "url": "build/workbox-sw.js",
+    "revision": "3fb3e299750822f4098267936945e759"
+  },
+  {
+    "url": "build/workbox-sw.prod.v2.1.2.js",
+    "revision": "685d1ceb6b9a9f94aacf71d6aeef8b51"
+  },
+  {
+    "url": "build/workbox-sw.prod.v2.1.2.js.map",
+    "revision": "8e170beaf8b748367396e6039c808c74"
+  },
+  {
+    "url": "index.html",
+    "revision": "a887aa5b7790ad156d2f23071a8443ae"
+  },
+  {
+    "url": "static/jquery.min.js",
     "revision": "0652da382b6fceb033dfe2b6c06d4d11"
   },
   {
-    "url": "logo.jpg",
+    "url": "static/logo.jpg",
     "revision": "71d9d86dc186fe1898f214428c61944e"
+  },
+  {
+    "url": "sw.js",
+    "revision": "2dab96ef1f9f59d015454ef38460447e"
+  },
+  {
+    "url": "workbox-cli-config.js",
+    "revision": "769eec9f27f63a04d0e9281cb34ec1ec"
   }
-];
-
-const workboxSW = new self.WorkboxSW();
-workboxSW.precache(fileManifest);
+])
