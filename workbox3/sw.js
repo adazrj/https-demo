1b581c156fba1216202ef18aa040b1507ac081b9
adarzheng <819591370@qq.com>
2018-02-08 10:33:20 +0800

    change preching

--- workbox3/sw.js
+++ workbox3/sw.js
@@ -5,33 +5,32 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-beta.0/
 
 if (workbox) {
   console.log('Yay! Workbox is loaded');
-  workbox.routing.registerRoute(
-	  // Cache CSS files
-	  /.*\.css/,
-	  // Use cache but update in the background ASAP
-	  workbox.strategies.staleWhileRevalidate({
-	    // Use a custom cache name
-	    cacheName: 'css-cache',
-	  })
-	);
+  workbox.precaching.preacheAndRoute([
+	    'static/test.css',
+	    'static/mm1.jpg',
+	    'index.html',
+	    { url: '/https-worker/workbox3/index.html', revision: '383676' },
+	]);
+ //  workbox.routing.registerRoute(
+	//   /.*\.css/,
+	//   workbox.strategies.staleWhileRevalidate({
+	//     cacheName: 'css-cache',
+	//   })
+	// );
 
-	workbox.routing.registerRoute(
-	  // Cache image files
-	  /.*\.(?:png|jpg|jpeg|svg|gif)/,
-	  // Use the cache if it's available
-	  workbox.strategies.cacheFirst({
-	    // Use a custom cache name
-	    cacheName: 'image-cache',
-	    plugins: [
-	      new workbox.expiration.Plugin({
-	        // Cache only 20 images
-	        maxEntries: 20,
-	        // Cache for a maximum of a week
-	        maxAgeSeconds: 7 * 24 * 60 * 60,
-	      })
-	    ],
-	  })
-	);
+	// workbox.routing.registerRoute(
+	//   /.*\.(?:png|jpg|jpeg|svg|gif)/,
+	//   workbox.strategies.cacheFirst({
+	//     cacheName: 'image-cache',
+	//     plugins: [
+	//       new workbox.expiration.Plugin({
+	//         maxEntries: 20,
+	//         maxAgeSeconds: 7 * 24 * 60 * 60,
+	//       })
+	//     ],
+	//   })
+	// );
+
 } else {
   console.log("Boo! Workbox didn't load");
 }
