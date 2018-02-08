4120dfb41dfe643a3aceb5518d2b93fa7ff9f539
ada <819591370@qq.com>
2018-02-08 08:54:10 +0800

    sw

--- workbox3/sw.js
+++ workbox3/sw.js
@@ -5,6 +5,10 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-beta.0/
 
 if (workbox) {
   console.log('Yay! Workbox is loaded');
+  workbox.routing.registerRoute(
+	  new RegExp('.*\.js'),
+	  workbox.strategies.networkFirst()
+	);
 } else {
   console.log("Boo! Workbox didn't load");
 }
